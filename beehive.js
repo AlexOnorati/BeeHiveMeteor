5e65c5f180191d885da2b8d4723e564feaf88eed
Alex Onorati <alex@onorati.com>
2016-02-03 18:53:46 -0800

    added beehive standards

--- beehive.js
+++ beehive.js
@@ -1,23 +1,83 @@
+Entries = new Mongo.Collection("entries");
+
+Router.route('/', function(){
+    this.render('beeinput'); // Show the beeinput template
+    this.layout('layout');
+});
+
+Router.route('/admin', function(){
+    this.render('admin'); // Show the admin template
+    this.layout('layout');
+});
+
+Router.route('/entries/:_id', function(){
+    this.render('entries', {
+        data: function(){
+            return Messages.findOne({_id: this.params._id});
+        }
+    });
+    this.layout('layout');
+},
+{
+    name: 'entries.show'
+}
+            
+);
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    
+    Meteor.subscribe("entries");
+    
+    Template.beeinput.events(
+        {
+          
+          
+        "submit form": function(event){
+            event.preventDefault();
+            
+            var nameBox = $(event.target).find('input[name=hivename]');
+            var name = nameBox.val();
+            
+            var countBox = $(event.target).find('input[name=mitecount]');
+            var count = countBox.val();
+            
+            var durationBox = $(event.target).find('input[name=duration]');
+            var duration = durationBox.val();
+            
+            var dateBox = $(event.target).find('input[name=collection]');
+            var date = dateBox.val();
+           
+            
+                Entries.insert(
+                {
+                    name: name,
+                    count: count,
+                    duration: duration,
+                    date: date,
+                    createdon: Date.now()
+                }
+            );
+        
+        }}
+  );
+    
+    Template.beeinput.helpers({
+        "entries": function(){
+            return Entries.find(
+                {}, 
+                {
+                    sort: 
+                    {
+                     createdon: -1
+                    }
+                }) || {};
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    
+    Meteor.publish("entries", function(){
+        return Entries.find();
+    })
 }
